1d6100876b968dae1bb6975d6721f0ca1cc67b55
effortw21 <tinna21abeba@gmail.com>
2026-08-25 08:58:02 +0300

    implementing module03 day 01 mini projects

---
+++ module-03-react-nextjs/mini-projects/day01/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Card from './components/Card'
+
+
+function App() {
+ 
+
+  return (
+    <>
+  <Header/>
+  <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ module-03-react-nextjs/mini-projects/day01/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="site-header">
+    
+      <h1 className="header-title">
+        Taste of <span className="highlight-text">Habesha</span>
+      </h1>
+    
+    </header>
+  )
+}
+
+export default Header
