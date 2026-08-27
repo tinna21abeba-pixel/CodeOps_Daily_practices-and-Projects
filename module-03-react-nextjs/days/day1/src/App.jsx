2240c83ccd2730bcf40d68bbc8d087d74bbfc7fb
effortw21 <tinna21abeba@gmail.com>
2026-08-27 06:30:29 +0300

    feat: module 03 day03 exercises on props and rendering methods

--- module-03-react-nextjs/days/day1/src/App.jsx
+++ module-03-react-nextjs/days/day1/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './components/Header.jsx'
 import Card from './components/Card.jsx'
+import Main from './components/Main.jsx'
 
 
 function App() {
   return (
     <>
       <Header />
-      <Card/>
+      <Main/>
       
     </>
   )
--- module-03-react-nextjs/days/day1/src/components/Card.jsx
+++ module-03-react-nextjs/days/day1/src/components/Card.jsx
@@ -1,24 +1,7 @@
 import React from 'react'
 
-
-const menu=[
-  {name:"dorowot",price:270},
-  {name:"shiro",price:200},
-  {name:"kitfo",price:300},
-  {name:"tibs",price:250},
-]
-
-function Card() {
-  return (
-    <div className="cards">
-    {menu.map((item)=>(
-        <div className="card">
-        <h2 className="name">{item.name}</h2>
-        <p className='price'>{item.price}</p>
-        </div>
-    ))}
-    </div>
-  )
+function Card({ children }) {
+  return <div className="card">{children}</div>
 }
 
 export default Card
